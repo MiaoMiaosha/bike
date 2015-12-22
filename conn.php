d9de7fe1c795a86620c52d98558455ac3a12fae6
TobyHan <630441063@qq.com>
2015-12-22 18:30:41 +0800

    modify conn.php

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
-$con = mysql_connect("localhost", "root", "");
+$con = mysql_connect("http://w.rdc.sae.sina.com.cn:3307", "w2o1lz024n", "42ili14h40wyl15zyx5h1lyjjyy0ijy4kyzjm014");
 if (!$con) {
 	die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("bike", $con);
+mysql_select_db("app_bikescu", $con);
 mysql_query('SET NAMES UTF8') or die("字符集设置错误");
  $siteName="四川大学公益自行车管理系统";
 ?>
